c46772463d81554a760ae69c19dcd361a0454de4
Sergey Orlov <orlov.home@gmail.com>
2025-12-30 21:14:12 +0300

    fix build

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-!#/bin/bash
+#!/bin/bash
 
 source .venv/bin/activate
-
 west build -s zmk/app -b nice_nano_v2 -d build/settings_reset -- -DSHIELD="settings_reset"
 west build -s zmk/app -b nice_nano_v2 -d build/right -- -DSHIELD="corne_right nice_oled"
 west build -s zmk/app -b nice_nano_v2 -d build/left -S studio-rpc-usb-uart -- -DSHIELD="corne_left nice_oled"
