5978c3709a996e1f8c5b9f9f3491d12886804956
Sergey Orlov <orlov.home@gmail.com>
2026-05-01 19:11:04 +0300

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 source .venv/bin/activate
+source  zephyr/zephyr-env.sh 
 west build -s zmk/app -b nice_nano_v2 -d build/settings_reset -- -DSHIELD="settings_reset"
 west build -s zmk/app -b nice_nano_v2 -d build/right -- -DSHIELD="corne_right nice_oled"
 west build -s zmk/app -b nice_nano_v2 -d build/left -S studio-rpc-usb-uart -- -DSHIELD="corne_left nice_oled"
